7747739c6a5a51b719267cd769a58663598252ba
ThUnDeR-CoDeR123 <dipnarayansen123@gmail.com>
2025-07-18 13:17:29 +0530

    previous code deleted

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  // Ensure Next.js can run behind a proxy
-  poweredByHeader: false,
-};
-
-module.exports = nextConfig;
